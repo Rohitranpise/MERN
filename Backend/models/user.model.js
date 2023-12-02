5c2ab91a77755b7367a42a2de0d7001fea66187a
Rohitranpise <rohitranpise2758@gmail.com>
2023-12-02 20:06:51 +0530

    created models

---
+++ Backend/models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+const schema = mongoose.Schema;
+
+const userSchema = new schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    pic: {
+        type: String,
+        required: true,
+        default: "https://t4.ftcdn.net/jpg/03/49/49/79/360_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.webp"
+    },
+
+}, { timestamps: true })
+
+const User = mongoose.model("User", userSchema)
+module.exports = User;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { Route, Router, Routes } from 'react-router-dom';
 import './App.css';
+import Homepage from './pages/Homepage';
+import Chatpage from './pages/Chatpage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+          <Route path='/' exact Component={Homepage} />
+          <Route path='/chats' exact Component={Chatpage} />
+        </Routes>
     </div>
   );
 }
