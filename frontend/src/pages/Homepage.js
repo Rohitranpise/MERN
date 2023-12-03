42ec9932fefa2b145131e13f76060d2cd627fdf5
Rohitranpise <rohitranpise2758@gmail.com>
2023-12-03 14:38:39 +0530

    signup template

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,8 +1,47 @@
 import React from 'react'
+import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/react'
+import Login from '../components/Authentication/Login'
+import Signup from '../components/Authentication/Signup'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <Container maxW='xl' centerContent>
+      <Box
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="work sans" color="black">Meet n Chat</Text>
+      </Box>
+      <Box
+        bg={"white"}
+        width="100%"
+        p={4}
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Tabs variant='soft-rounded'>
+          <TabList marginBottom="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Signup</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
   )
 }
 
